8266fa36b5bc7efd6ed537b7d42d111224c8bbfa
Yaroslav Kuznetsov <asklater@bk.ru>
2025-02-04 19:48:40 +0300

    fixed codestyle, changed integer node id to size_t

--- third_party/dotter.hpp
+++ third_party/dotter.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <vector>
 #include <map>
 #include <cstring>
 #include <fstream>
 
-namespace dotter
-{
-    enum COLORS
-    {
+namespace dotter {
+    enum COLORS {
         BLACK   = 0,
         WHITE   = 1,
         RED     = 2,
@@ -19,19 +14,16 @@ namespace dotter
         GREEN   = 5
     }; // enum COLORS
     
-    class NodeStyle
-    {
-public:
-        enum SHAPES
-        {
+    class NodeStyle {
+    public:
+        enum SHAPES {
             BOX         = 0,
             ELLIPSE     = 1,
             TRIANGLE    = 2,
             DIAMOND     = 3
         }; // enum SHAPES
 
-        enum STYLES
-        {
+        enum STYLES {
             SOLID   = 0,
             ROUNDED = 1,
             BOLD    = 3
@@ -42,8 +34,7 @@ public:
 
         NodeStyle() {}
 
-        void set_style(SHAPES shape, STYLES style, COLORS color, COLORS fill_color, COLORS font_color)
-        {
+        void SetStyle(SHAPES shape, STYLES style, COLORS color, COLORS fill_color, COLORS font_color) {
             shape_      = shape;
             style_      = style;
             color_      = color;
@@ -51,7 +42,7 @@ public:
             font_color_ = font_color;
         }
 
-public:
+    public:
         SHAPES shape_       = SHAPES::BOX;
         STYLES style_       = STYLES::ROUNDED;
         COLORS color_       = COLORS::BLACK;
@@ -60,11 +51,9 @@ public:
 
     }; // class NodeStyle
 
-    class LinkStyle
-    {
-public:
-        enum STYLES
-        {
+    class LinkStyle {
+    public:
+        enum STYLES {
             SOLID   = 0,
             DASHED  = 1,
             BOLD    = 2
@@ -74,43 +63,39 @@ public:
 
         LinkStyle() {}
 
-        void set_style(STYLES style, COLORS color)
-        {
+        void SetStyle(STYLES style, COLORS color) {
             style_ = style;
             color_ = color;
         }
 
-public:
+    public:
         STYLES style_ = STYLES::BOLD;
         COLORS color_ = COLORS::BLACK;
     }; // class LinkStyle
 
-    class Node
-    {
-public:
-        Node(NodeStyle &style, std::string text, int id) : style_(style), text_(text), id_(id) {}
+    class Node {
+    public:
+        Node(NodeStyle &style, std::string text, std::size_t id) : style_(style), text_(text), id_(id) {}
 
-public:
+    public:
         NodeStyle style_;
         std::string text_;
-        int id_;
+        std::size_t id_;
     }; // class Node
 
     class Link
     {
-public:
-        Link(LinkStyle &style, std::string text, int node1_id, int node2_id) :
+    public:
+        Link(LinkStyle &style, std::string text, std::size_t node1_id, std::size_t node2_id) :
         style_(style), text_(text), node1_id_(node1_id), node2_id_(node2_id) {}
-
-public:
+    public:
         LinkStyle style_;
         std::string text_;
-        int node1_id_;
-        int node2_id_;
+        std::size_t node1_id_;
+        std::size_t node2_id_;
     }; // class Link
 
-    static std::map<COLORS, std::string> colors_map = 
-    {
+    static std::map<COLORS, std::string> colors_map = {
         {COLORS::RED,    "red"   },
         {COLORS::BLUE,   "blue"  },
         {COLORS::BLACK,  "black" },
@@ -119,111 +104,93 @@ public:
         {COLORS::YELLOW, "yellow"}
     };
 
-    static std::map<NodeStyle::SHAPES, std::string> node_shapes_map = 
-    {
+    static std::map<NodeStyle::SHAPES, std::string> node_shapes_map = {
         {NodeStyle::SHAPES::BOX,      "box"     },
         {NodeStyle::SHAPES::DIAMOND,  "diamond" },
         {NodeStyle::SHAPES::ELLIPSE,  "ellipse" },
         {NodeStyle::SHAPES::TRIANGLE, "triangle"}
     };
 
-    static std::map<NodeStyle::STYLES, std::string> node_styles_map = 
-    {
-        {NodeStyle::STYLES::BOLD, "bold"},
+    static std::map<NodeStyle::STYLES, std::string> node_styles_map = {
+        {NodeStyle::STYLES::BOLD,    "bold"   },
         {NodeStyle::STYLES::ROUNDED, "rounded"},
-        {NodeStyle::STYLES::SOLID, "solid"}
+        {NodeStyle::STYLES::SOLID,   "solid"  }
     };
 
-    static std::map<LinkStyle::STYLES, std::string> link_styles_map = 
-    {
-        {LinkStyle::STYLES::BOLD, "bold"},
+    static std::map<LinkStyle::STYLES, std::string> link_styles_map = {
+        {LinkStyle::STYLES::BOLD,   "bold"  },
         {LinkStyle::STYLES::DASHED, "dashed"},
-        {LinkStyle::STYLES::SOLID, "solid"}
+        {LinkStyle::STYLES::SOLID,  "solid" }
     };
 
-    class Dotter
-    {
-public:
-        Dotter()
-        {
+    class Dotter {
+    public:
+        Dotter() {
             current_dot_ += "digraph DotGraph\n{\n";
         }
 
-        void set_node_style(NodeStyle::SHAPES shape, NodeStyle::STYLES style,
-                            COLORS color, COLORS fill_color, COLORS font_color)
-        {
-            current_node_style_.set_style(shape, style, color, fill_color, font_color);
+        void SetNodeStyle(NodeStyle::SHAPES shape, NodeStyle::STYLES style,
+                            COLORS color, COLORS fill_color, COLORS font_color) {
+            current_node_style_.SetStyle(shape, style, color, fill_color, font_color);
         }
 
-        void set_link_style(LinkStyle::STYLES style, COLORS color)
-        {
-            current_link_style_.set_style(style, color);
+        void SetLinkStyle(LinkStyle::STYLES style, COLORS color) {
+            current_link_style_.SetStyle(style, color);
         }
 
-        void add_node(std::string text, int id)
-        {
+        void AddNode(std::string text, std::size_t id) {
             nodes_.push_back({current_node_style_, text, id});
         }
 
-        void add_node(std::string text, int id, NodeStyle style)
-        {
+        void AddNode(std::string text, std::size_t id, NodeStyle style) {
             nodes_.push_back({style, text, id});
         }
 
-        void add_node(std::string text, int id,
+        void AddNode(std::string text, std::size_t id,
                       NodeStyle::SHAPES shape, NodeStyle::STYLES style,
-                      COLORS color, COLORS fill_color, COLORS font_color)
-        {
+                      COLORS color, COLORS fill_color, COLORS font_color) {
             NodeStyle new_style{shape, style, color, fill_color, font_color};
             nodes_.push_back({new_style, text, id});
         }
 
-        void add_link(int node1_id, int node2_id)
-        {
+        void AddLink(std::size_t node1_id, std::size_t node2_id) {
             links_.push_back({current_link_style_, "", node1_id, node2_id});
         }
 
-        void add_link(int node1_id, int node2_id, LinkStyle style)
-        {
+        void AddLink(std::size_t node1_id, std::size_t node2_id, LinkStyle style) {
             links_.push_back({style, "", node1_id, node2_id});
         }
 
-        void add_link(std::string text, int node1_id, int node2_id)
-        {
+        void AddLink(std::string text, std::size_t node1_id, std::size_t node2_id) {
             links_.push_back({current_link_style_, text, node1_id, node2_id});
         }
 
-        void add_link(std::string text, int node1_id, int node2_id,
-                      LinkStyle::STYLES style, COLORS color)
-        {
+        void AddLink(std::string text, std::size_t node1_id, std::size_t node2_id,
+                      LinkStyle::STYLES style, COLORS color) {
             LinkStyle new_style{style, color};
             links_.push_back({new_style, text, node1_id, node2_id});
         }
 
-        void add_link(int node1_id, int node2_id,
-                      LinkStyle::STYLES style, COLORS color)
-        {
+        void AddLink(std::size_t node1_id, std::size_t node2_id,
+                      LinkStyle::STYLES style, COLORS color) {
             LinkStyle new_style{style, color};
             links_.push_back({new_style, "", node1_id, node2_id});
         }
 
-        void print_dot_text(std::ostream& stream)
-        {
-            generate_dot_text();
+        void PrintDotText(std::ostream& stream) {
+            GenerateDotText();
             stream << current_dot_;
         }
 
-        void print_dot_text(std::string dot_file_name="graph.dot")
-        {
-            generate_dot_text();
+        void PrintDotText(std::string dot_file_name = "graph.dot") {
+            GenerateDotText();
             std::ofstream out;
             out.open(dot_file_name);
             out << current_dot_;
             out.close();
         }
 
-        void render(const char* dot_file_name="graph.dot", const char* image_file_name="graph.png")
-        {
+        void Render(const char* dot_file_name = "graph.dot", const char* image_file_name = "graph.png") {
             char str1[100] = "dot -Tpng ";
             char str2[] = " -o ";
             strcat(str1, dot_file_name);
@@ -232,11 +199,9 @@ public:
             system(str1);
         }
         
-private:
-        void generate_dot_text()
-        {
-            for (auto& node : nodes_)
-            {
+    private:
+        void GenerateDotText() {
+            for (auto& node : nodes_) {
                 current_dot_ += "\tNode" + std::to_string(node.id_) + " [";
                 current_dot_ += "shape=\"" + node_shapes_map[node.style_.shape_] + "\", ";
                 current_dot_ += "color=\"" + colors_map[node.style_.color_] + "\", ";
@@ -248,8 +213,7 @@ private:
                 current_dot_ += "];\n";
             } 
 
-            for (auto& link : links_)
-            {
+            for (auto& link : links_) {
                 current_dot_ += "\tNode" + std::to_string(link.node1_id_) + " -> " +
                                 "Node" + std::to_string(link.node2_id_) + " [";
                 current_dot_ += "color=\"" + colors_map[link.style_.color_] + "\", ";
@@ -262,14 +226,11 @@ private:
             current_dot_ += "}\n";
         }
 
-private:
+    private:
         std::string current_dot_;
         NodeStyle   current_node_style_;
         LinkStyle   current_link_style_;
-
         std::vector<Node> nodes_;
         std::vector<Link> links_;
-
     }; // class Dotter
-
 } // namespace dotter
