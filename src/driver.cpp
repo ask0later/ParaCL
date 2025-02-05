6cdb2000feef24238989187a9530a9d0f8d18e0f
Yaroslav Kuznetsov <asklater@bk.ru>
2025-02-05 18:12:50 +0300

    added exceptions catching

--- src/driver.cpp
+++ src/driver.cpp
@@ -3,11 +3,13 @@
 // here we can return non-zero if lexing is not done inspite of EOF detected
 int yyFlexLexer::yywrap() { return 1; }
 
-int main() {
+int main() try {
     FlexLexer *lexer = new yyFlexLexer;
     yy::Driver driver(lexer);
     driver.parse();
     driver.DrawAST();
     driver.Execute();
     delete lexer;
-}
+} catch (std::exception &ex) {
+    std::cout << ex.what() << std::endl;
+};
