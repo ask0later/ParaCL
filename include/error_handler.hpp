b63e79eaafd0be1ad66d523e0381317fc1b419e2
Yaroslav Kuznetsov <asklater@bk.ru>
2025-02-23 20:05:51 +0300

    changed std::string& to std::string_view

--- include/error_handler.hpp
+++ include/error_handler.hpp
@@ -14,10 +14,10 @@ public:
         return error_handler;
     }
 
-    std::string GetFullErrorMessage(std::string error_name, \
-                                    std::string error_mes, \
+    std::string GetFullErrorMessage(std::string_view error_name, \
+                                    std::string_view error_mes, \
                                     const yy::Location &loc) const { 
-        auto mes = error_name + ": " + error_mes + ", at line #" + std::to_string(loc.begin.line) + ":\n"; 
+        auto mes = std::string(error_name) + ": " + std::string(error_mes) + ", at line #" + std::to_string(loc.begin.line) + ":\n"; 
         mes += (loc.GetCodeLine(loc.begin.line - 1) + "\n");
         mes += std::string(loc.begin.column - 1, ' ');
         mes += std::string(loc.end.column - loc.begin.column, '^');
